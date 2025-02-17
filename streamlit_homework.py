5a290fd414ce33a05c29ef4b2472cea532802f99
danyb111 <dany.b111@hotmail.com>
2025-02-16 21:39:17 -0600

    Codigo para csv y json

--- streamlit_homework.py
+++ streamlit_homework.py
@@ -1,13 +1,32 @@
 # %%
 
 import streamlit as st
+import pandas as pd
 
-# %% Codigo para la pagina de streamlit
+# %% Codigo para la pagina de Streamlit
 
 st.write("""
-# Sales model
-Below are our sale predictions for this customer.
+# Visualizador de archivos CSV y JSON
+Sube el archivo que quieres visualizar.
          """)
+
+# %% Cargando archivo
+
+file = st.file_uploader('Sube un archivo CSV o JSON', type = ['csv', 'json'])
+
+if file is not None:
+    if file.name.endswith('.csv'):
+        df = pd.read_csv(file)
+    if file.name.endswith('json'):
+        df = pd.read_json(file)
+
+
+    # Mostrar el DataFrame
+
+    st.write('Contenido del archivo')
+    st.dataframe(df)
+
+
 
 # Correr esto en la terminal
 

70623c77d8da311a40369d119d2781faf2c129ee
danyb111 <dany.b111@hotmail.com>
2025-02-16 19:38:02 -0600

    Primer commit

---
+++ s.py
@@ -0,0 +1,14 @@
+# %%
+
+import streamlit as st
+
+# %% Codigo para la pagina de streamlit
+
+st.write("""
+# Sales model
+Below are our sale predictions for this customer.
+         """)
+
+# %% Correr la app
+
+streamlit run streamlit_homework.py
